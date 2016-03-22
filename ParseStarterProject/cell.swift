6f111479e1f99ab40181c389f3b2ad8c3d2d940f
Shyamal Anadkat <shyamalanadkat@troysmac-2.local>
2016-03-22 10:53:12 -0500

    - working table and cell view for view images

---
+++ ParseStarterProject/cell.swift
@@ -0,0 +1,18 @@
+//
+//  cell.swift
+//  ParseStarterProject-Swift
+//
+//  Created by Shyamal Anadkat on 2016-03-21.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+
+class cell: UITableViewCell {
+    //for each object in the cell 
+    
+    @IBOutlet var postedImage: UIImageView!
+    @IBOutlet var username: UILabel!
+    
+    @IBOutlet var message: UILabel!
+}
