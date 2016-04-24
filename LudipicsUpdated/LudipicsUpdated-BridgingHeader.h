605174d034db0d4fd51211bea6ae8a91d923cb22
Akshansh Thakur <akshanshthakur@Akshanshs-MacBook-Pro.local>
2016-04-24 18:43:43 -0500

    init commit

---
+++ LudipicsUpdated/LudipicsUpdated-BridgingHeader.h
@@ -0,0 +1,18 @@
+//
+//  LudipicsUpdated-Bridging-Header.h
+//  LudipicsUpdated
+//
+//  Created by Akshansh Thakur on 4/22/16.
+//  Copyright © 2016 Akshansh Thakur. All rights reserved.
+//
+#import "CameraFoundationVC.h"
+#import "GroupVC.h"
+#import "GroupTableCellVC.h"
+#import <Parse/Parse.h>
+#import <Bolts/BFTask.h>
+
+#ifndef LudipicsUpdated_Bridging_Header_h
+#define LudipicsUpdated_Bridging_Header_h
+
+
+#endif /* LudipicsUpdated_Bridging_Header_h */
