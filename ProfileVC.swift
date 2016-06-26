57488348c90ab3f0570b6907e3390886f385170f
srajmohan <rajmohan@wisc.edu>
2016-06-25 22:15:56 -0500

    Updated
    Updated profile page #14

--- ProfileVC.swift
+++ ProfileVC.swift
@@ -17,12 +17,14 @@ class ProfileVC: UIViewController {
     @IBOutlet var settingsButton: UIBarButtonItem!
     
     @IBOutlet weak var profileBar: UINavigationItem!
-    
+
     @IBOutlet var profilePicture: UIImageView!
     
     @IBOutlet weak var toolbar1: UIToolbar!
     
     @IBOutlet var coverPicture: UIImageView!
+    
+    weak var borderLayer: CAShapeLayer?
     
     func onePixelImageWithColor(color : UIColor) -> UIImage {
         let colorSpace = CGColorSpaceCreateDeviceRGB()
@@ -32,7 +34,7 @@ class ProfileVC: UIViewController {
         CGContextFillRect(context, CGRectMake(0, 0, 1, 1))
         let image = UIImage(CGImage: CGBitmapContextCreateImage(context)!)
         return image
-    }    
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,13 +45,8 @@ class ProfileVC: UIViewController {
             self.settingsButton.setTitleTextAttributes([NSFontAttributeName: font], forState: UIControlState.Normal)
         }
         
-        profilePicture.layer.borderWidth = 1
-        profilePicture.layer.masksToBounds = false
-        profilePicture.layer.borderColor = UIColor.whiteColor().CGColor
-        profilePicture.layer.cornerRadius = profilePicture.frame.height/2
-        profilePicture.clipsToBounds = true
         
-        let bgImageColor = UIColor.whiteColor().colorWithAlphaComponent(0.6)
+        let bgImageColor = UIColor.whiteColor().colorWithAlphaComponent(0.4)
         
         toolbar1.setBackgroundImage(onePixelImageWithColor(bgImageColor),
                                    forToolbarPosition: UIBarPosition.Bottom,
@@ -57,11 +54,22 @@ class ProfileVC: UIViewController {
         
         profileTab.addBottomBorderWithColor(UIColor.lightGrayColor(), width: 0.5)
         
-        //let image:UIImage = UIImage(named: "Top Header")!
-        //self.navigationItem.titleView = UIImageView(image: image)
         
         UITabBarItem.appearance().titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -12)
         UITabBarItem.appearance().setTitleTextAttributes([NSFontAttributeName: UIFont.systemFontOfSize(18)], forState: .Normal)
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
+        super.viewDidLayoutSubviews()
+        
+        profilePicture.layer.borderWidth = 0.5
+        profilePicture.layer.masksToBounds = false
+        profilePicture.layer.borderColor = UIColor.whiteColor().CGColor
+        
+        profilePicture.layer.cornerRadius = profilePicture.frame.width/2
+        profilePicture.clipsToBounds = true
         
     }
     
